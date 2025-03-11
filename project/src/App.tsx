0e51aa8ec0138eb18f4fc4b30d1d433b27d35c07
shristy-02 <shristyasv2004@gmail.com>
2025-03-11 12:42:31 +0530

    Add some change

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -21,8 +21,8 @@ function App() {
           <Header />
           <main className="flex-grow">
             <Routes>
-              {/* <Route path="/" element={<Home />} /> */}
-              {/* <Route path="/about" element={<About />} /> */}
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/login" element={<LoginForm />} />
               <Route path="/register" element={<RegisterForm />} />
