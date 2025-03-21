37968fb079ccf1c0308bac8bec5da0994837f40b
shekhar <shekhar@acstechconsulting.com>
2025-03-21 16:46:07 +0530

    db connection

--- Backend/iete/settings.py
+++ Backend/iete/settings.py
@@ -97,15 +97,20 @@ WSGI_APPLICATION = 'iete.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "iete",
-        "USER": "root",
-        "PASSWORD": "shalender",
-        "HOST": "localhost",
-        "PORT": "3306",
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": "iete",
+    #     "USER": "root",
+    #     "PASSWORD": "shalender",
+    #     "HOST": "localhost",
+    #     "PORT": "3306",
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
